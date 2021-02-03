148995ec4920abb4e23e696bb2ce83e846f4db6c
wuyanzu45 <1169563282@qq.com>
2021-02-03 23:07:42 +0800

    better

---
+++ exe_1_22/test.c
@@ -0,0 +1,40 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+
+	int a = -5;
+	printf("%d\n", a / 2);
+	//int b = 2;
+	//int c = 0;
+	//c = a++, ++a;
+	//printf("c = %d", c);
+	//printf("a = %d", a);
+	//int a = 1;
+	//printf("%d\n", sizeof(a));
+	//printf("%d\n", sizeof(int));
+	////int a = 2;
+	//int b = 3;
+	//int c = 0,d = 0;
+	//c = (++a) * b + (a++) * b;
+	//d = (++a) * b + (a++) * b + a * b;
+	//printf("%d\n", c);
+	//printf("%d\n", d);
+	//int a = 2;
+	//printf("%u\n", sizeof(a));
+	//printf("%u\n", sizeof(+a));
+	//printf("%u\n", sizeof(-a));
+	//printf("%u\n", sizeof(!1));
+	//printf("%u\n", sizeof(1));
+	//int a = 10;
+	//int b = 20;
+	//int max;
+	//max = (a > b ? a++ : b++);
+	//printf("a = %d\n", a);
+	//printf("b = %d\n", b);
+	//printf("max = %d\n", max);
+	//int a;
+	//scanf("%d", &a);
+	//printf("%d\n", a);
+	return 0; 
+}

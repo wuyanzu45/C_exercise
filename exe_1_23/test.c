cfc9f1f4e280813b5a53d66a906a071cd3adfd56
wuyanzu45 <1169563282@qq.com>
2021-02-04 13:48:50 +0800

    do it！

---
+++ exe_1_23/test.c
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+	int a = -1;
+	//int count = 0;
+	//while (a)
+	//{
+	//	count++;
+	//	a &= a - 1;
+	//}
+	//printf("%d\n", count);
+
+	//int sum = 0;
+	//for (int i = 0; i < 32; i++)
+	//{
+	//	sum += (a >> i) & 1;
+	//}
+	//printf("%d\n", sum);
+
+	//int a = 10;
+	//short b;
+	//b = a + 5;
+	//printf("%d\n", b);
+	//printf("%d\n", sizeof b);
+	return 0;
+}
